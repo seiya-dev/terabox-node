cde2b78c6934030bf99ad763f4f5e82eeeffc1ed
seiya-git <seiya-git@users.noreply.github.com>
2025-05-05 18:21:14 +0300

    update meta info app

--- app/app-filemeta.js
+++ app/app-filemeta.js
@@ -4,6 +4,7 @@ import fs from 'node:fs';
 import path from 'node:path';
 import { fileURLToPath } from 'url';
 import { filesize } from 'filesize';
+import { fetch } from 'undici';
 
 import Argv from './module-argv.js';
 import TeraBoxApp from 'terabox-api';
@@ -98,7 +99,23 @@ async function showMeta(rPath, pathData){
         console.log('File :', f.server_filename);
         console.log('Size :', f.size);
         console.log('Size :', filesize(f.size, {standard: 'iec', round: 2}));
-        // console.log('DLink:', f.dlink);
+        
+        console.log();
+        console.log('Fetching Hashes...');
+        const hashReq = await fetch(f.dlink, {
+            method: 'HEAD',
+            headers:{
+                'User-Agent': app.params.ua,
+            },
+            redirect: 'follow',
+            signal: AbortSignal.timeout(app.TERABOX_TIMEOUT * 2),
+        });
+        const crc32 = parseInt(hashReq.headers.get('x-bs-meta-crc32'));
+        const md5hash = hashReq.headers.get('content-md5');
+        
+        console.log('CRC32:', crc32, '(int)');
+        console.log('CRC32:', crc32.toString(16).toUpperCase().padStart(8, '0'), '(hex)');
+        console.log('MD5  :', md5hash);
         console.log();
     }
 } 
