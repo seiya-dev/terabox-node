94de122e598f174546d01bb50b70c0d8c3bb3762
seiya-git <seiya-git@users.noreply.github.com>
2025-05-22 15:30:43 +0300

    update

--- app/app-uploader.js
+++ app/app-uploader.js
@@ -230,7 +230,7 @@ async function uploadDir(localDir, remoteDir){
             console.log(`:: Upload chunks...`);
         }
         
-        const maxTasks = data.size <= 8 * Math.pow(1024, 3) ? 10 : 5;
+        const maxTasks = data.size <= 4 * Math.pow(1024, 3) ? 10 : 5;
         const upload_status = await uploadChunks(app, data, filePath, maxTasks);
         delete data.uploaded;
         
